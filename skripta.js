8e15458c65858ec54c387a82df4590dfbe6a7a73
sp0ng3bob <matej.defaveri@gmail.com>
2016-03-18 15:33:05 +0000

    Dinamika gumba za ustavitev stroboskopa

--- skripta.js
+++ skripta.js
@@ -4,13 +4,12 @@ window.addEventListener('load', function() {
 	//Dodaj novo barvo
 	var dodajBarvo = function(event) {
 		var input = document.createElement('button');
-        var picker = new jscolor(input);
+        var picker = new jscolor(input); //jscolor je definiran v 'stroboskop.html'
         picker.fromRGB(Math.floor(Math.random()*255), Math.floor(Math.random()*255), Math.floor(Math.random()*255));
         document.getElementById("barve").appendChild(input);
 	};
 	
-	document.querySelector("#novaBarva") 
-		.addEventListener('click', dodajBarvo);
+	document.querySelector("#novaBarva").addEventListener('click', dodajBarvo);
 		
 	//Odstrani barve
 	var odstraniB = document.getElementById("odstraniBarve");
@@ -32,21 +31,28 @@ window.addEventListener('load', function() {
 
 		if (ustavi) {
 			ustavi = false;
-		} else {
-			novId = (id+1) % vrednosti.length;
-			timeout = Math.floor((Math.random() * (maxCas-minCas)) + minCas);
+		} 
+		else {
+			var novId = (id+1) % vrednosti.length;
+			var timeout = Math.floor((Math.random() * (maxCas-minCas)) + minCas);
 			setTimeout(function() {spremeniBarvo(novId)} , timeout);
-		}		
+		}	
+		
 	};
 	
 	var stop = function(event) {
 		ustavi = true;
+		var start = document.querySelector ("#start");
+		start.innerHTML = "Zaženi stroboskop";
+		start.removeEventListener ('click', stop);
+		start.addEventListener ('click', zagon);
+
 	};
 	
 	var zagon = function(event) {
 		vrednosti = [];
 		var barve = document.querySelectorAll("#barve > button");
-		for (i = 0; i < barve.length; i++) {
+		for (var i = 0; i < barve.length; i++) {
 			var barva = barve[i];
 			vrednosti.push(barva.innerHTML);
 		}
@@ -59,6 +65,7 @@ window.addEventListener('load', function() {
 		start.innerHTML = "Ustavi stroboskop";
 		start.removeEventListener('click', zagon);
 		start.addEventListener('click', stop);
+		
 	};
 	
 	document.querySelector("#start").addEventListener('click', zagon);
